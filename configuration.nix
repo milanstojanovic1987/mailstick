d282eec14f23ed9dd5409a5ee00de15a10d8cc60
milans213 <110895013+milanstojanovic1987@users.noreply.github.com>
2025-05-28 17:26:43 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -20,33 +20,78 @@
   };
 
   ###########################
-  # Boot-loader (USB stick) #
+  # Filesystems             #
   ###########################
-  boot.loader.systemd-boot.enable        = true;
-  boot.loader.efi.canTouchEfiVariables   = true;
+  fileSystems."/" = {
+    device = "/dev/mapper/cryptroot";
+    fsType  = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/BOOT";
+    fsType  = "vfat";
+  };
+
+  fileSystems."/persist" = {
+    device = "/dev/disk/by-label/DATA";
+    fsType  = "ext4";
+  };
+
+  fileSystems."/home" = {
+    fsType  = "tmpfs";
+    options = [ "size=256M" ];
+  };
+
+  ###########################
+  # Bootloader (GRUB)       #
+  ###########################
+  boot.loader.systemd-boot.enable = false;    # we’ll use GRUB instead
+  boot.loader.grub = {
+    enable                = true;
+    version               = 2;
+    devices               = [ "/dev/sdc" ];  # your USB device
+    efiSupport            = true;
+    efiInstallAsRemovable = true;
+  };
 
   ###########################
   # Tor onion mail relay    #
   ###########################
-  services.tor = {
-    enable   = true;
-    settings = {
-      HiddenServiceDir     = "/persist/tor/hidden_service_mail";
-      HiddenServiceVersion = 3;
-      HiddenServicePort    = [
-        "25 127.0.0.1:2525"
-        "587 127.0.0.1:1587"
-        "993 127.0.0.1:1993"
-      ];
-    };
+  services.tor.enable   = true;
+  services.tor.settings = {
+    HiddenServiceDir     = "/persist/tor/hidden_service_mail";
+    HiddenServiceVersion = 3;
+    HiddenServicePort    = [
+      "25  127.0.0.1:2525"
+      "587 127.0.0.1:1587"
+      "993 127.0.0.1:1993"
+    ];
   };
-
   system.activationScripts.torPersist = {
     text = ''
       mkdir -p /persist/tor
       rm -rf /var/lib/tor
       ln -sf /persist/tor /var/lib/tor
     '';
+  };
+
+  ###########################
+  # Ensure mailuser exists  #
+  ###########################
+  users.groups.mailuser = { };               # create mailuser group
+  users.users.mailuser = {
+    isSystemUser = true;                     # no shell login
+    group        = "mailuser";
+    home         = "/var/lib/mail";
+    shell        = pkgs.runCommandNoCC "nologin" {};
+  };
+
+  ###########################
+  # Pre-create postfix spool#
+  ###########################
+  environment.etc."var/spool/postfix" = {
+    target = "/var/spool/postfix";
+    mode   = "0755";
   };
 
   ###########################
@@ -63,41 +108,20 @@
   services.dovecot2.mailLocation = "maildir:~/Maildir";
 
   ###########################
-  # User account            #
+  # Persistence wiring      #
   ###########################
-  users.users.mailuser = {
-    isNormalUser = true;
-    extraGroups  = [ "wheel" ];
-  };
-
-  ###########################
-  # Persistent mail storage #
-  ###########################
-  fileSystems."/persist" = {
-    device = "/dev/disk/by-label/DATA";
-    fsType = "ext4";
-  };
-
   system.activationScripts.persist = {
     text = ''
-      mkdir -p /persist/{postfix,dovecot}
+      mkdir -p /persist/{postfix,dovecot,gpg}
+      chown mailuser:mailuser /persist/gpg
+
+      # link services into persist
       ln -sf /persist/postfix    /var/spool/postfix
       ln -sf /persist/dovecot    /var/lib/dovecot
+      ln -sf /persist/gpg        /home/mailuser/.gnupg
+
       mkdir -p /home/mailuser/Maildir
-
-      # Persist GPG keyring
-      mkdir -p /persist/gpg
-      chown mailuser:mailuser /persist/gpg
-      ln -sf /persist/gpg       /home/mailuser/.gnupg
     '';
-  };
-
-  ###########################
-  # Make /home ephemeral    #
-  ###########################
-  fileSystems."/home" = {
-    fsType  = "tmpfs";
-    options = [ "size=256M" ];
   };
 
   ###########################
