d217f17454e497b3d20ed3c0eadc7453d13175e7
milans213 <110895013+milanstojanovic1987@users.noreply.github.com>
2025-06-04 16:05:38 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,28 +1,37 @@
 {
   description = "MailStick hardened Tor relay (NixOS 25.05)";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-    in
-    {
-      nixosConfigurations.mailstick-vbox = nixpkgs.lib.nixosSystem {
+
+      # --- 1. Running VM / USB config (what you use now) -----------------
+      vmConfig = nixpkgs.lib.nixosSystem {
         inherit system;
-        # <- hand the pinned nixpkgs path to the module system
         specialArgs = { inherit nixpkgs; };
-        modules = [ ./configuration.nix ];
+        modules     = [ ./configuration.nix ];
       };
 
-      packages.${system} = {
-        isoImage = self.nixosConfigurations.mailstick-vbox
-          .config.system.build.isoImage;
+      # --- 2. Installer ISO ------------------------------------------------
+      isoConfig = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit nixpkgs; };
+        modules = [
+          ./configuration.nix
+          # Adds the bootable ISO builder
+          "${nixpkgs}/nixos/modules/installer/cd-dvd/iso-image.nix"
+        ];
+      };
+    in
+    {
+      # For nixos-rebuild
+      nixosConfigurations.mailstick-vbox = vmConfig;
 
-        vmImage = self.nixosConfigurations.mailstick-vbox
-          .config.system.build.vmImage;
+      packages.${system} = {
+        # Bootable ISO (EFI + BIOS)
+        mailstick-iso = isoConfig.config.system.build.isoImage;
       };
     };
 }
